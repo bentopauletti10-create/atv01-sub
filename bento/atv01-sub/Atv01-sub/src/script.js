738675a431089e33dd2a28660c7cbc31b7f14108
bentopauletti10-create <bentopauletti10@gmail.com>
2026-05-25 11:24:06 -0300

    Add files via upload

---
+++ bento/atv01-sub/Atv01-sub/src/script.js
@@ -0,0 +1,11 @@
+function twitter(){
+    window.location.href = "https://twitter.com";
+}
+
+function instagram(){
+    window.location.href = "https://instagram.com";
+}
+
+function youtube(){
+    window.location.href = "https://youtube.com";
+}
